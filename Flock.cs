2d3567b67d09a78097f4679891f051c373869d97
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-27 18:50:01 +0530

    init flock script

--- Flock.cs
+++ Flock.cs
@@ -4,10 +4,52 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    public FlockAgent agentPrefab;
+    List<FlockAgent> agents = new List<FlockAgent>();
+    public FlockBehavior behavior;
+
+    [Range(10, 500)]
+    public int startingCount = 250;
+    const float AgentDensity = 0.08f;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 10; // Since the behaviors are going to be minute we need to multiply it by a value 
+
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+
+    // Neighbour distances
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    float squareMaxSpeed;
+    float squareNeighborRadius;
+    float squareAvoidanceRadius;
+    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        // init flock
+        for(int i = 0; i < startingCount; i++)
+        {
+            FlockAgent newAgent = Instantiate(
+                agentPrefab,
+                Random.insideUnitCircle*startingCount*AgentDensity,
+                Quaternion.Euler(Vector3.forward*Random.Range(0f,360f)),
+                transform
+                );
+            newAgent.name = "Agent " + i;
+            agents.Add(newAgent);
+        }
     }
 
     // Update is called once per frame
